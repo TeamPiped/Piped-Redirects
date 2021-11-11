6af1f4942903fa7fe63956a982adce6baf527838
FireMaskterK <20838718+FireMasterK@users.noreply.github.com>
2021-11-11 08:29:57 +0000

    Rewrite Invidious instances to Piped.
    Closes #11

--- js/background.js
+++ js/background.js
@@ -1,12 +1,22 @@
 window.browser = window.browser || window.chrome;
 
+const INVIDIOUS_INSTANCES = [];
+
+fetch("https://api.invidious.io/instances.json")
+	.then((resp) => resp.json())
+	.then((array) => array.forEach((json) => INVIDIOUS_INSTANCES.push(json[0])));
+
 browser.webRequest.onBeforeRequest.addListener(
 	(details) => {
 		const url = new URL(details.url);
 		if (url.hostname.endsWith("youtu.be") && url.pathname.length > 1) {
 			return { redirectUrl: "https://piped.kavin.rocks/watch?v=" + url.pathname.substr(1) };
 		}
-		if (url.hostname.endsWith("youtube.com") || url.hostname.endsWith("youtube-nocookie.com")) {
+		if (
+			url.hostname.endsWith("youtube.com") ||
+			url.hostname.endsWith("youtube-nocookie.com") ||
+			INVIDIOUS_INSTANCES.includes(url.hostname)
+		) {
 			url.hostname = "piped.kavin.rocks";
 			return { redirectUrl: url.href };
 		}
